9f39c99fb41e268401f93654a333a048c9dba259
Alexandra Havia <sasha.havia@gmail.com>
2018-07-05 14:50:24 -0400

    Fixed Move action

--- game/index.js
+++ game/index.js
@@ -1,43 +1,66 @@
-import {Map} from 'immutable'
+import { Map } from 'immutable';
 
-
-let board = Map()
+// let board = Map();
 
 // ACTIONS
-const MOVE = 'MOVE'
-const START = 'START'
+const MOVE = 'MOVE';
+const START = 'START';
 
 // ACTION CREATORS
-export const move = (turn, [row, col]) => {
+export const move = (turn, position) => {
   return {
     type: MOVE,
-    position: [row, col],
-    player: turn
-  }
-}
+    position,
+    player: turn,
+  };
+};
 
 export const start = () => {
   return {
-    type: START
-  }
-}
+    type: START,
+  };
+};
 
 const initialState = {
-  board: board,
-  turn: 'X'
-}
+  board: Map(),
+  turn: 'X',
+};
+
+// Function winner
+const streak = (board, firstCoord, secondCoord, thirdCoord) => {
+  // check if (0,0), (0,1), (0,2) are all X's
+  // if(board[[firstCoord[0],firstCoord[1]] ){
+  // }
+  // state.board is a map object
+};
+const winner = board => {
+  // for (let r = 0; r != 3; ++r) {
+  //   for (let c = 0; c != 3; ++c) {
+  //   }
+  // }
+  // horizontal
+  // call streak with coordinates (0,0), (0,1), (0,2)
+  // call streak with coordinates (1,0), (1,1), (1,2)
+  // call streak with coordinates (2,0), (2,1), (2,2)
+  // vertical
+  // call streak with coordinates (0,0), (1,0), (2,0)
+  // call streak with coordinates (0,1), (1,1), (2,1)
+  // call streak with coordinates (0,2), (1,2), (2,2)
+  // diagonal
+  // call streak with coordinates (0,0), (1,1), (2,2)
+  // call streak with coordinates (0,2), (1,1), (2,0)
+};
 
 export default function reducer(state = initialState, action) {
+  const newState = Object.assign({}, state);
   switch (action.type) {
-    case START :
-      return state;
-    case MOVE :
-      return {
-        board: state.board.setIn(action.position, action.player),
-        turn: state.turn === 'X' ? 'O' : 'X'
-      }
+    case MOVE:
+      // console.log('board', state.board.get());
+      // console.log('board', state.board.get(state.board, 2));
+      newState.board = newState.board.setIn(action.position, action.player);
+      newState.turn = newState.turn === 'X' ? 'O' : 'X';
+      return newState;
     default:
       return state;
   }
 }
-
